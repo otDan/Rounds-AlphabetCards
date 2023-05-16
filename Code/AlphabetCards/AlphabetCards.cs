ac19b8f82d3519a20dabec8abb40c6a4f14fc5da
otDan <otdanofficial@gmail.com>
2023-05-16 11:49:35 +0200

    feat: Initial setup

---
+++ Code/AlphabetCards/AlphabetCards.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnboundLib.GameModes;
+using UnityEngine;
+
+[BepInDependency("com.willis.rounds.unbound")]
+[BepInDependency("pykess.rounds.plugins.moddingutils")]
+[BepInDependency("io.olavim.rounds.rwf")]
+[BepInPlugin(ModId, CompatibilityModName, Version)]
+[BepInProcess("Rounds.exe")]
+public class AlphabetCards : BaseUnityPlugin
+{
+    private const string ModId = "ot.dan.rounds.alphabetcards";
+    private const string ModName = "Alphabet Cards";
+    public const string Version = "1.0.0";
+    public const string ModInitials = "GS";
+    private const string CompatibilityModName = "AlphabetCards";
+    public static AlphabetCards Instance { get; private set; }
+
+    internal void Awake()
+    {
+        Instance = this;
+
+        var harmony = new Harmony(ModId);
+        harmony.PatchAll();
+
+        // assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+    }
+
+    internal void Start()
+    {
+
+    }
+
+    public void Log(string debug)
+    {
+        Logger.LogInfo(debug);
+    }
+}
---
+++ Code/AlphabetCards/CardHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnboundLib.Cards;
+using UnityEngine;
+
+public class CardHolder: MonoBehaviour 
+{
+    public List<GameObject> Cards;
+    public List<GameObject> HiddenCards;
+
+    internal void RegisterCards()
+    {
+        var mod = AlphabetCards.ModInitials;
+        foreach(var Card in Cards)
+        {
+            CustomCard.RegisterUnityCard(Card, mod, Card.GetComponent<CardInfo>().cardName, true, null);
+        }
+
+        foreach(var Card in HiddenCards)
+        {
+            CustomCard.RegisterUnityCard(Card, mod, Card.GetComponent<CardInfo>().cardName, false, null);
+            ModdingUtils.Utils.Cards.instance.AddHiddenCard(Card.GetComponent<CardInfo>());
+        }
+    }
+}
