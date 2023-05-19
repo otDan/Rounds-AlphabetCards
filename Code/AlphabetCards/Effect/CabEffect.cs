5c25c019b2317a03d0b77daf56e156d93164b3ee
otDan <otdanofficial@gmail.com>
2023-05-19 20:58:42 +0200

    feat: Almost finished cards with effects

---
+++ Code/AlphabetCards/Effect/CabEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabEffect : MonoBehaviour
+{
+    private Player player;
+    private GameObject cabSprite;
+    private SpriteRenderer renderer;
+
+    void Start()
+    {
+        player = base.transform.root.GetComponent<Player>();
+        var art = player.transform.GetChild(0);
+        var cab = AlphabetCards.assets.LoadAsset<GameObject>("Sprite_CAB");
+        cabSprite = GameObject.Instantiate(cab, art);
+        renderer = cabSprite.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+        if (cabSprite == null) return;
+        if (renderer == null) return;
+
+        var flipped = player.data.aimDirection.x >= 0;
+
+        if (flipped)
+        {
+            cabSprite.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            cabSprite.transform.localPosition = new Vector3(0.65f, 0, 0);
+        }
+        else
+        {
+            cabSprite.transform.localRotation = new Quaternion(0, 180f, 0, 0);
+            cabSprite.transform.localPosition = new Vector3(-0.65f, 0, 0);
+        }
+    }
+}
---
+++ Code/AlphabetCards/Effect/GunEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEffect : MonoBehaviour
+{
+    private Player player;
+    private GameObject gunSprite;
+    private SpriteRenderer renderer;
+
+    void Start()
+    {
+        player = base.transform.root.GetComponent<Player>();
+        var art = player.data.weaponHandler.gun.gameObject.transform.GetChild(1);
+        var gun = AlphabetCards.assets.LoadAsset<GameObject>("Sprite_GUN");
+        gunSprite = GameObject.Instantiate(gun, art);
+        renderer = gunSprite.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+        if (gunSprite == null) return;
+        if (renderer == null) return;
+
+        var flipped = player.data.aimDirection.x >= 0;
+        renderer.flipY = !flipped;
+    }
+}
